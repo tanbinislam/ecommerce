a7f93995e99b81db8c4db4a1135cfda1c33d5971
tanbinislam <tanbinislamridoy@gmail.com>
2021-12-24 16:13:36 +0600

    updated user seeder to create an admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = \App\Models\User::create(
+            [
+                'name' => 'Tanbin Islam',
+                'email' => 'tanbinislamridoy@gmail.com',
+                'user_name' => 'tanbin123456',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ]
+        );
+
+        $role = Role::findOrCreate('Admin');
+        $role2 = Role::findOrCreate('Customer');
+        $admin->assignRole($role, $role2);
+
+        \App\Models\User::factory(300)->create();
+    }
+}
