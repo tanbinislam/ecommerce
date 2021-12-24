5ebcf4b6c334796d065614873a2a92a6f7fd580f
tanbinislam <tanbinislamridoy@gmail.com>
2021-12-24 16:20:13 +0600

    Added Admin Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
+
 require __DIR__.'/auth.php';
+Route::get('/shop', [AdminController::class, 'adminDashboard'])->name('shop');
+Route::get('/shop/users', [AdminController::class, 'users'])->name('allUsers');
+Route::get('/shop/user/add', [AdminController::class, 'addUser'])->name('addUser');
+Route::post('/shop/user/create', [AdminController::class, 'createUser'])->name('createUser');
+Route::get('/shop/user/edit/{user}', [AdminController::class, 'editUser'])->name('editUser');
+Route::post('/shop/user/update/{user}', [AdminController::class, 'updateUser'])->name('updateUser');
+Route::get('/shop/user/view/{user}', [AdminController::class, 'viewUser'])->name('viewUser');
+Route::get('/shop/user/trash', [AdminController::class, 'viewSoftDeletedUsers'])->name('trashedUsers');
+Route::post('/shop/user/delete/{user}', [AdminController::class, 'softDeleteUser'])->name('deleteUser');
+Route::post('/shop/user/restore/{id}', [AdminController::class, 'restoreUser'])->name('restoreUser');
+Route::post('/shop/user/permanent-delete/{id}', [AdminController::class, 'permanentDeleteUser'])->name('permanentDeleteUser');
