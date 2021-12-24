06233fbe00a3a1fee064c3a9a7d51cc00eb75ab3
tanbinislam <tanbinislamridoy@gmail.com>
2021-12-24 16:19:35 +0600

    updated css & js links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,22 @@
         <meta charset="utf-8" />
         <title>{{ config('app.name', 'Laravel') }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- App favicon -->
-        <link rel="shortcut icon" href="{{asset('')}}images/favicon.ico">
+        <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}">
+
+        
+        <!-- third party css -->
+        <link href="{{asset('css/dataTables.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('css/responsive.bootstrap5.min.css')}}" rel="stylesheet" type="text/css" />
+        <!-- third party css end -->
+
         <!-- App css -->
-        <link href="{{asset('')}}css/icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="{{asset('')}}css/app-modern.min.css" rel="stylesheet" type="text/css" id="light-style" />
-        <link href="{{asset('')}}css/app-modern-dark.min.css" rel="stylesheet" type="text/css" id="dark-style" />
+        <link href="{{asset('css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('css/app-modern.min.css')}}" rel="stylesheet" type="text/css" id="light-style" />
+        <link href="{{asset('css/app-modern-dark.min.css')}}" rel="stylesheet" type="text/css" id="dark-style" />
+        @stack('css')
+        <link href="{{asset('css/main.css')}}" rel="stylesheet" type="text/css" />
         
     </head>
 
@@ -91,18 +101,17 @@
 
 
         <!-- bundle -->
-        <script src="{{asset('')}}js/vendor.min.js"></script>
-        <script src="{{asset('')}}js/app.min.js"></script>
-
+        <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+        <script src="{{asset('js/select2.min.js')}}"></script>
+        <script src="{{asset('js/moment.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('js/app.min.js')}}"></script>
         <!-- third party js -->
-        <script src="{{asset('')}}js/vendor/apexcharts.min.js"></script>
-        <script src="{{asset('')}}js/vendor/jquery-jvectormap-1.2.2.min.js"></script>
-        <script src="{{asset('')}}js/vendor/jquery-jvectormap-world-mill-en.js"></script>
+        <script src="{{asset('js/datatables.min.js')}}"></script>
+        <script src="{{asset('js/sweetalert.min.js')}}"></script>
         <!-- third party js ends -->
-
-        <!-- demo app -->
-        <script src="{{asset('')}}js/pages/demo.dashboard.js"></script>
-        <!-- end demo js-->
+        @stack('scripts')
+        <script src="{{asset('js/main.js')}}"></script>
         
     </body>
 </html>
